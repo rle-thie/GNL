e9527937c0af3f9e2c7bf60b1f36b6ce2adba9ae
Robin LE THIEC <rle-thie@e2r7p12.clusters.42paris.fr>
2021-12-22 17:34:23 +0100

    ouais enfaite il faut utiliser la finction read

--- get_next_line.c
+++ get_next_line.c
@@ -6,25 +6,22 @@
 /*   By: rle-thie <rle-thie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 14:59:30 by rle-thie          #+#    #+#             */
-/*   Updated: 2021/12/22 15:42:51 by rle-thie         ###   ########.fr       */
+/*   Updated: 2021/12/22 17:31:16 by rle-thie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
-
-static int	k = 0;
+#include <stdlib.h>
 
 char	*get_next_line(int fd)
 {
-	FILE	*fp;
 	
-	fp = fopen("/test.txt", "r");
-	fclose(fp);
 }
 
-int main(void)
+int	main(void)
 {
-	printf("%s\n", get_next_line("test.txt"));
+	get_next_line(BUFFER_SIZE);
+	printf("%d", BUFFER_SIZE);
 	return (0);
 }
