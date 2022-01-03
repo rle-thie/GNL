732decababcdf40b430a2e35c961081d0c2ae455
Robin LE THIEC <rle-thie@e2r7p11.clusters.42paris.fr>
2022-01-03 15:56:52 +0100

    utils plus .h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: rle-thie <rle-thie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 15:18:39 by rle-thie          #+#    #+#             */
-/*   Updated: 2021/12/24 17:26:28 by rle-thie         ###   ########.fr       */
+/*   Updated: 2022/01/03 15:48:19 by rle-thie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,9 @@
 
 #include <unistd.h>
 
-void	ft_putstr(char *str);
-void	ft_putchar(char c);
 size_t	ft_strlen(char *str);
+char	*get_next_line(int fd);
+char    *ft_strchr(const char *s, int c);
+char    *ft_strjoin(char const *s1, char const *s2);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,25 +6,11 @@
 /*   By: rle-thie <rle-thie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 15:19:19 by rle-thie          #+#    #+#             */
-/*   Updated: 2021/12/24 17:26:24 by rle-thie         ###   ########.fr       */
+/*   Updated: 2022/01/03 15:48:57 by rle-thie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_putstr(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		ft_putchar(str[i++]);
-}
 
 size_t	ft_strlen(char *str)
 {
@@ -34,4 +20,41 @@ size_t	ft_strlen(char *str)
 	while(str[i])
 		i++;
 	return (i);
+}
+
+char    *ft_strchr(const char *s, int c)
+{
+        size_t  i;
+
+        i = 0;
+        while (s[i] && s[i] != (char)c)
+                i++;
+        if (s[i] == (char)c)
+                return ((char *)&s[i]);
+        else
+                return (NULL);
+}
+
+char    *ft_strjoin(char const *s1, char const *s2)
+{
+        size_t  lentt;
+        char    *tab;
+        size_t  i;
+        size_t  y;
+
+        if (!s1 || !s2)
+                return (NULL);
+        i = 0;
+        y = 0;
+        lentt = ft_strlen(s1) + ft_strlen(s2);
+        tab = malloc(sizeof(char) * (lentt + 1));
+        if (!tab)
+                return (NULL);
+        while (s1[y])
+                tab[i++] = s1[y++];
+        y = 0;
+        while (s2[y])
+                tab[i++] = s2[y++];
+        tab[i] = '\0';
+        return (tab);
 }
